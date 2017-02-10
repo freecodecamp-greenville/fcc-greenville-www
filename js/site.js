0f70ec3da02ff4e7b348a25f06a1740a437cd56e
Daniel Jones <tortxof@gmail.com>
2017-02-10 00:23:38 -0500

    Display message from xhr response.

--- js/site.js
+++ js/site.js
@@ -3,8 +3,11 @@ var doorbellMessage = document.getElementById('doorbell-message')
 
 doorbellButton.addEventListener('click', function() {
   var xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', function() {
-    doorbellMessage.innerText = "We got you'r message"
+  xhr.addEventListener('load', function(e) {
+    var response = JSON.parse(e.target.response)
+    if (response.hasOwnProperty('message')) {
+      doorbellMessage.innerText = response.message
+    }
   })
   xhr.open('GET', 'http://doorbell.fcc-greenville.com/')
   xhr.send()
