4446768461f73def938cbd693170dff3eb2c5640
Daniel Jones <tortxof@gmail.com>
2018-08-09 20:29:23 -0400

    Output css in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  entry: ['./css/main.css', './js/index.js'],
+  entry: { styles: './css/main.css', app: './js/index.js' },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: __dirname + '/src'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'style-loader'
-          },
+          devMode
+            ? { loader: 'style-loader' }
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: '/'
+                }
+              },
           {
             loader: 'css-loader',
             options: {
