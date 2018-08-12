67245285d71a9e3974ad7986b54bf29ccb9b6adf
Daniel Jones <tortxof@gmail.com>
2018-08-12 13:28:07 -0400

    Add axios and babel

--- js/index.js
+++ js/index.js
@@ -1,49 +1,36 @@
-document
-  .getElementById('show-future-meetups')
-  .addEventListener('click', function(e) {
-    document.querySelector('.future-meetups').style.display = 'block';
-    var elShowMeetups = document.getElementById('show-future-meetups');
-    elShowMeetups.parentElement.removeChild(elShowMeetups);
-    e.preventDefault();
-  });
+import axios from 'axios';
+
+document.getElementById('show-future-meetups').addEventListener('click', e => {
+  document.querySelector('.future-meetups').style.display = 'block';
+  e.target.parentElement.removeChild(e.target);
+  e.preventDefault();
+});
 
 var doorbellButton = document.getElementById('doorbell');
 var doorbellMessage = document.getElementById('doorbell-message');
 
 doorbellButton.addEventListener('click', function() {
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', function(e) {
-    var response = JSON.parse(e.target.response);
-    if (response.hasOwnProperty('message')) {
-      doorbellMessage.innerText = response.message;
-    }
-  });
-  xhr.open('POST', 'https://doorbell.fcc-greenville.com/');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(
-    JSON.stringify({
-      action: 'ring'
-    })
-  );
+  axios
+    .post('https://doorbell.fcc-greenville.com/', { action: 'ring' })
+    .then(response => {
+      if (response.status === 200 && response.data.hasOwnProperty('message')) {
+        doorbellMessage.innerText = response.data.message;
+      }
+    });
 });
 
 function getDoorbellStatus() {
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', function(e) {
-    var response = JSON.parse(e.target.response);
-    if (response.hasOwnProperty('status')) {
-      if (response.status === 'active') {
+  axios
+    .post('https://doorbell.fcc-greenville.com/', { action: 'status' })
+    .then(response => {
+      if (
+        response.status === 200 &&
+        response.data.hasOwnProperty('status') &&
+        response.data.status === 'active'
+      ) {
         doorbellButton.style.display = 'block';
       }
-    }
-  });
-  xhr.open('POST', 'https://doorbell.fcc-greenville.com/');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(
-    JSON.stringify({
-      action: 'status'
-    })
-  );
+    });
 }
 
 getDoorbellStatus();
