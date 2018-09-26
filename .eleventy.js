e21d273e09ee3274bfe71ffe11455f33b1dfc40f
Daniel Jones <tortxof@gmail.com>
2018-09-26 02:10:41 -0400

    Adjust date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/app.js');
   eleventyConfig.addPassthroughCopy('src/styles.js');
   eleventyConfig.addPassthroughCopy('src/styles.css');
   eleventyConfig.addPassthroughCopy('src/admin/config.yml');
-  eleventyConfig.addFilter('timeToDate', time => new Date(time).toISOString());
+
+  eleventyConfig.addFilter('timeToDate', time =>
+    DateTime.fromJSDate(new Date(time))
+      .setZone('America/New_York')
+      .toFormat('EEEE, MMMM d, h:mm a')
+  );
 
   return {
     dir: {
