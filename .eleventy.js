e09d38f7b08e3559027c954d28708e4b6306abf9
Daniel Jones <tortxof@gmail.com>
2018-09-26 01:10:56 -0400

    Remove passthroughFileCopy

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/admin/config.yml');
 
   return {
-    passthroughFileCopy: true,
     dir: {
       input: 'src',
       output: 'dist'
